8c83b13a0edda81b3953f9ebf80c941a42952605
ÆtherSurfer <aethersurfer@aethercraft.com>
2014-05-21 13:23:00 -0400

    Largest prime factor

---
+++ 3.scala
@@ -0,0 +1,4 @@
+val target = 600851475143L
+val sqrtOfTarget =  Math.sqrt(target)
+lazy val primes: Stream[Int] = 2 #:: Stream.from(3).filter(i => primes.takeWhile{j => j * j <= i}.forall{ k => i % k > 0});
+primes.takeWhile(_ < sqrtOfTarget).filter(target % _ == 0) toList
