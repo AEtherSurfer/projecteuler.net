d70c9a083d920cf9ee7d4624bec323b04244f753
ÆtherSurfer <aethersurfer@aethercraft.com>
2014-05-30 16:01:11 -0400

    Summation of primes

---
+++ 10.scala
@@ -0,0 +1,3 @@
+//Summation of primes
+lazy val primes: Stream[Int] = 2 #:: Stream.from(3).filter(i => primes.takeWhile{j => j * j <= i}.forall{ k => i % k > 0})
+primes.takeWhile(_ < 2000000).foldLeft(0L)(_+_)
